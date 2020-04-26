0ac1edffddd55ebcf2d5f82c4ed0dc509ebbea3f
pradeep89jai@gmail.com <reshu@nexgin.com>
2020-04-26 14:56:34 +0530

    inital setupscafold

---
+++ blogUseContext/App.js
@@ -0,0 +1,12 @@
+import React from 'React';
+import { View ,Text } from 'react-native';
+
+const App =() =>{
+<>
+<View><Text>this is basic blog app with </Text> 
+<Text>Context</Text>
+</View>
+</>  
+}
+
+export default App;
